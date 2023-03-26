2c6a481781ed1e7b98c52bb12a7d9e8f0c5ed97e
Felipe Pereira <felipejiti@hotmail.com>
2023-03-26 12:22:05 -0300

    unico jeito que eu consegui

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ if a >=0:
 else :
      print(f'e um número negativo = {a}')
 
-print('não está executando minhas condições')
+#Exercicio 2 , tentei fazer os executaveis mais deu tanto erro que desisti kkk .
+
+b= float(input('digite quantas unidades vai querar comprar , temos uma promoção que ao comprar mais de meia duzias a maça sai por 1 real'))
+
+if b >=12:
+  c = b*1
+  print(f'sua compra foi na promoção, o valor foi {c}')
+else:
+    print(f'sua compra foi pelo preço normal de 1.30 cada, sua compra deu ='  , b*1.30)

e59390115942fe40e2bbb5eb3d84b68b3d601bf7
Felipe Pereira <felipejiti@hotmail.com>
2023-03-26 11:11:20 -0300

    primeiro exercicio

---
+++ main.py
@@ -0,0 +1,7 @@
+a = int(input('digite um numero qualquer '))
+if a >=0:
+     print(f'e um número positivo = {a}')
+else :
+     print(f'e um número negativo = {a}')
+
+print('não está executando minhas condições')

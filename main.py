ed2150f80b65ac436d271bb642321dc1e7ce408c
Felipe Pereira <felipejiti@hotmail.com>
2023-04-16 12:08:35 -0300

    exeercicio do palindromo

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-a = int(input('digite um numero qualquer '))
-if a >=0:
-     
-     print(f'e um número positivo = {a}')
-else :
-     print(f'e um número negativo = {a}')
-
-#Exercicio 2 , tentei fazer os executaveis mais deu tanto erro que desisti kkk .
-
-b= float(input('digite quantas unidades vai querar comprar , temos uma promoção que ao comprar mais de meia duzias a maça sai por 1 real'))
-
-if b >=12:
-  c = b*1
-  print(f'sua compra foi na promoção, o valor foi {c}')
+a = input("digite uma frase")
+reverse_string = a [::-1]
+if a == reverse_string:
+    print('é um palindromo')
 else:
-    print(f'sua compra foi pelo preço normal de 1.30 cada, sua compra deu ='  , b*1.30)
-    #
+    print('não é um palindromo')
